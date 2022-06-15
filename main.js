2af089b37c8fcb8edfd2702c4d408d61c7145422
Adrien Graux <adrien.graux@octo.com>
2022-06-15 22:28:07 +0200

    api key solution

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ import {formations} from "./data/formations.data.js";
 const app = express()
 const port = 3000
 
+const API_KEY = '395a9cce-6ffc-48b8-8372-b95089c3abc6'
+
+
 app.get('/formations', (req, res) => {
+  const apiKeyFromHeader = req.headers['x-api-key']
+  if (API_KEY !== apiKeyFromHeader) {
+    res.status(401).send({code: 401, error: 'FORBIDDEN'})
+    return
+  }
   const result = formations().map((formation) => ({id: formation.id}))
   res.send(result)
 })
