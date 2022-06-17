be109a745055b3887723e0d690ccd3dcbf7cf716
Adrien Graux <adrien.graux@octo.com>
2022-06-17 16:15:14 +0200

    jwt controle

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 import express from 'express'
+import {Issuer} from 'openid-client'
+import {createRemoteJWKSet, jwtVerify} from 'jose'
 import {formations} from "./data/formations.data.js";
 
 const app = express()
 const port = 3000
 
-app.get('/formations', (req, res) => {
+app.get('/formations', async (req, res) => {
+  const authorization = req.headers['authorization']
+  const accessToken = authorization?.replace(/bearer /gi, '')
+  const issuer = await Issuer.discover("https://dev-tzeom-9o.us.auth0.com")
+  const jwksUrl = new URL(issuer.metadata.jwks_uri)
+  const jwks = createRemoteJWKSet(jwksUrl)
+
+  try {
+    const {payload} = await jwtVerify(accessToken, jwks)
+    console.log(payload)
+
+  } catch (e) {
+    console.log(e)
+    return res.status(401).send({code: 401, error: 'FORBIDDEN'})
+  }
   const result = formations().map((formation) => ({id: formation.id}))
   res.send(result)
 })
