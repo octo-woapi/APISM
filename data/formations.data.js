d98f79886286d8ff7b2c128cf2c53f964f54a0f0
Adrien Graux <adrien.graux@octo.com>
2022-06-23 17:28:24 +0200

    thematique formations

---
+++ data/formations.data.js
@@ -0,0 +1,14 @@
+export const formations = () => ([
+  {
+    id: "SAPI1",
+    nom: "Ouvrir son SI et développer son modèle d’affaires"
+  },
+  {
+    id: "APISM",
+    nom: 'Sécuriser et Manager son API'
+  },
+  {
+    id: "MREST",
+    nom: 'Développer des microservices REST'
+  }
+])
--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-const express = require('express')
+import express from 'express'
+import {formations} from "./data/formations.data.js";
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  const result = {
-    success: true
-  }
+app.get('/formations', (req, res) => {
+  const result = formations().map((formation) => ({id: formation.id}))
   res.send(result)
 })
 
+app.get('/formations/:id', (req, res) => {
+  const id = req.params.id;
+  const result = formations().find((formation) => formation.id === id)
+  if (result) {
+    res.send(result)
+  } else {
+    res.status(404).send({code: 404, error: 'NOT_FOUND'})
+  }
+})
+
 app.listen(port, () => {
-  console.log(`Dummy api listening on port ${port}`)
+  console.log(`Formations api listening on port ${port}`)
 })
